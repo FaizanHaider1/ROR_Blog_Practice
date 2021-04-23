befe6e14391b6bb7dc1ded33e08661a15a0c38f5
Faizan Haider <faizan.haider@aldaimsolutions.com>
2021-04-23 12:18:30 +0500

    Message added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,26 @@
 class ContactsController < ApplicationController
-  
+    def index
+      @contacts = Contact.all
+    end
+
     def new
       @contact = Contact.new
     end
+    def show
+      @contact = Contact.find(params[:id])
+    end
   
     def create
-      @contact = Contact.new
+      @contact = Contact.new(contact_params)
   
       if @contact.save
-        render html: "<script>alert('Message sent successfully')</script>".html_safe
-        @contact = Contact.new
-        # redirect_to(new_contact_path(@contact))
-      else
-        render :new
+        render html: "<script>alert('Message sent successfully #{contact_params}')</script>".html_safe
+        # redirect_to index_contact_path
+        return
       end
+    end
+    private
+    def contact_params
+      params.require(:contact).permit(:subject, :email, :message )
     end
 end
